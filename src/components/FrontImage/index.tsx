9b2279aeb54ffb97cc745a3694d1b59439fdbf9f
Henrik Hørlück Berg <henrik@horluck.no>
2019-10-29 17:13:36 +0100

    Fix FrontImage css to more closely match prev. ver

--- src/components/FrontImage/index.tsx
+++ src/components/FrontImage/index.tsx
@@ -1,5 +1,6 @@
 import frontLogo from "img/in_logo_medium.png";
 import React, { FC } from "react";
+import style from "./style.module.scss";
 
 interface IProps {
   image: string;
@@ -14,7 +15,7 @@ const FrontImage: FC<IProps> = ({ image, title, undertitle, className }) => (
     className={`front-img ${className}`}
   >
     <img src={frontLogo} alt="" />
-    <h1 className="front-img-title">{title}</h1>
+    <h1 className={style.frontImgTitle}>{title}</h1>
     {undertitle && <h3>{undertitle}</h3>}
   </div>
 );
